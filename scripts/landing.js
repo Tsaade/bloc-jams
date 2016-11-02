11af2135ce5b15169bbba7b56fd63d635c3154f4
Tarek Saade <Tarek Saade>
2016-11-02 13:26:24 -0400

    Checkpoint 9: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,6 @@
-alert("Why hello there! I'm a wee bit of JavaScript.");
+var pointArray = document.getElementsByClassName('point');
 
-var animatePoints = function() {
- 
-    var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
  
     var revealPoint = function(index) {
         points[index].style.opacity = 1;
@@ -14,3 +12,18 @@ var animatePoints = function() {
         revealPoint(i);
     };
 };
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointArray);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointArray);
+        }
+    });
+}
